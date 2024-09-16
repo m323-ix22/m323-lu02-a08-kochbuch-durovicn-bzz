2923eb60328157d6398522ced8d7f04f0c09aae0
durov <durovicn@bzz.ch>
2024-09-16 14:17:29 +0200

    .

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-# Dein Code kommt hier hin
+"""
+Dieses Modul passt die Mengenangaben eines Rezepts an eine neue Anzahl von Personen an,
+basierend auf einem JSON-String, der das Rezept darstellt. Es verwendet ausschließlich
+Immutable Data und Pure Functions.
+"""
+
 import json
 
 
+# Funktion zur Anpassung des Rezepts
 def adjust_recipe(recipee, new_servingss):
+    """
+    Diese Funktion passt die Mengenangaben eines Rezepts an die neue Anzahl von Personen an.
+
+    Args:
+        recipee (dict): Das ursprüngliche Rezept als Python-Dictionary.
+        new_servingss (int): Die neue Anzahl der Personen.
+
+    Returns:
+        dict: Ein neues Rezept-Dictionary mit angepassten Mengenangaben.
+    """
     # Berechne den Faktor, mit dem die Mengen angepasst werden
     factor = new_servingss / recipee['servings']
 
@@ -17,7 +33,17 @@ def adjust_recipe(recipee, new_servingss):
     }
 
 
+# Funktion zum Laden eines Rezepts aus einem JSON-String
 def load_recipe(recipe_jsonn):
+    """
+    Diese Funktion wandelt einen JSON-kodierten String in ein Python-Dictionary um.
+
+    Args:
+        recipe_jsonn (str): Der Rezept-String im JSON-Format.
+
+    Returns:
+        dict: Das Rezept als Python-Dictionary.
+    """
     return json.loads(recipe_jsonn)
 
 
@@ -25,7 +51,8 @@ if __name__ == '__main__':
     # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = ('{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, '
                    '"Minced Meat": 500}, "servings": 4}')
-    # Dein Code kommt hier hin
+
+    # Lade das Rezept
     recipe = load_recipe(recipe_json)
 
     # Neue Anzahl an Personen
@@ -35,5 +62,5 @@ if __name__ == '__main__':
     adjusted_recipe = adjust_recipe(recipe, new_servings)
 
     # Ausgabe des angepassten Rezepts
-    print("Originales Rezept:", recipe)
-    print("Angepasstes Rezept für", new_servings, "Personen:", adjusted_recipe)
+    print('Originales Rezept:', recipe)
+    print('Angepasstes Rezept für', new_servings, 'Personen:', adjusted_recipe)
